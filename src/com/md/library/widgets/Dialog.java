a9545dfb6941ca5ad52192a80b420a92ef3b5f3b
wangjun <elexjun@gmail.com>
2014-12-26 12:44:43 +0800

    update simple dialog

--- src/com/md/library/widgets/Dialog.java
+++ src/com/md/library/widgets/Dialog.java
@@ -24,63 +24,40 @@ import android.widget.TextView;
 public abstract class Dialog extends android.app.Dialog {
 	
 	protected Context mContext;
-	private String mTitle;
-	private String mMessage;
+	protected int mLayout;	
 	
-	private RelativeLayout mRootView;
-	private RelativeLayout mDialogView;
+	protected RelativeLayout mRootView;
+	protected RelativeLayout mDialogView;
 	
-	private TextView mTitleView;
-	private TextView mMessageView;
-	
-	private FlatButton mAcceptButton;
-	private FlatButton mCancelButton;
-	
-	View.OnClickListener onAcceptButtonClickListener;
-	View.OnClickListener onCancelButtonClickListener;
 
 	private int mBackgroundColor = Color.parseColor("#ffffff");
 
-	public Dialog(Context context,String title, String message) {
+	public Dialog(Context context, int layout) {
 		super(context, android.R.style.Theme_Translucent);
-		mContext = context;
-		mTitle = title;
-		mMessage = message;
+		mLayout = layout;
 	}
+	
+	
+//	public Dialog(Context context, int layout, String title, String message) {
+//		super(context, android.R.style.Theme_Translucent);
+//		mContext = context;
+//		mTitle = title;
+//		mMessage = message;
+//	}
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
-	    setContentView(R.layout.dialog);
+	    setContentView(mLayout);
 	    
-	    initializeViews();
-	    initializeBackground();
-	    initializeEvents();
-	    
-	    setTitle(mTitle);
-	    setMessage(mMessage);
-	}
-	
-	
-	private void initializeViews() {
 	    mDialogView = (RelativeLayout)findViewById(R.id.contentDialog);
 	    mRootView = (RelativeLayout)findViewById(R.id.dialog_rootView);
-	    mTitleView = (TextView) findViewById(R.id.title);
-	    mMessageView = (TextView) findViewById(R.id.message);
-	    mAcceptButton = (FlatButton) findViewById(R.id.button_accept);
-	    mCancelButton = (FlatButton)findViewById(R.id.button_cancel);
-	}
-	
-	private void initializeBackground() {
-		LayerDrawable layer = (LayerDrawable) mDialogView.getBackground();
-		GradientDrawable shape = (GradientDrawable) layer.findDrawableByLayerId(R.id.shape_bacground);
-		shape.setColor(mBackgroundColor);			
-	}
-	
-	private void initializeEvents() {
-		mRootView.setOnTouchListener(new OnTouchListener() {
+	    
+	    initializeBackground();
+	    
+	    mRootView.setOnTouchListener(new OnTouchListener() {
 			
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
@@ -94,63 +71,18 @@ public abstract class Dialog extends android.app.Dialog {
 				return false;
 			}
 		});
-		
-		mAcceptButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dismiss();
-				if(onAcceptButtonClickListener != null)
-			    	onAcceptButtonClickListener.onClick(v);
-			}
-		});
-	    
-	    mCancelButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dismiss();
-				if(onCancelButtonClickListener != null)
-					onCancelButtonClickListener.onClick(v);
-			}
-		});
-	}
-	
-	public void setTitle(String title) {
-		if(null == title || title.length() == 0) {
-			mTitleView.setVisibility(View.GONE);
-		} else {
-			mTitleView.setVisibility(View.VISIBLE);
-			mTitleView.setText(title);
-		}
-	}
-	
-	public void setMessage(String message) {
-		if(null == message || message.length() == 0) {
-			mMessageView.setVisibility(View.GONE);
-		}else {
-			mMessageView.setVisibility(View.VISIBLE);
-			mMessageView.setText(message);
-		}
-	}
-	
-	
-	public void setAcceptText(String text) {
-		mAcceptButton.setText(text);
 	}
 
-	public void setCancelText(String text) {
-		mCancelButton.setText(text);
+	
+	public void setBackground(int color) {
+		mBackgroundColor = color;
+		initializeBackground();
 	}
 	
-	public void setOnAcceptButtonClickListener(View.OnClickListener onAcceptButtonClickListener) {
-		this.onAcceptButtonClickListener = onAcceptButtonClickListener;
-		if(mAcceptButton != null)
-			mAcceptButton.setOnClickListener(onAcceptButtonClickListener);
-	}
-
-	public void setOnCancelButtonClickListener( View.OnClickListener onCancelButtonClickListener) {
-		this.onCancelButtonClickListener = onCancelButtonClickListener;
-		if(mCancelButton != null)
-			mCancelButton.setOnClickListener(onCancelButtonClickListener);
+	private void initializeBackground() {
+		LayerDrawable layer = (LayerDrawable) mDialogView.getBackground();
+		GradientDrawable shape = (GradientDrawable) layer.findDrawableByLayerId(R.id.shape_bacground);
+		shape.setColor(mBackgroundColor);			
 	}
 	
 	
--- src/com/md/library/widgets/SimpleDialog.java
+++ src/com/md/library/widgets/SimpleDialog.java
@@ -1,10 +1,118 @@
 package com.md.library.widgets;
 
+import com.md.library.R;
+import com.md.library.views.FlatButton;
+
 import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SimpleDialog extends Dialog {
+	
+	private String mTitle;
+	private String mMessage;
+	
+	private TextView mTitleView;
+	private TextView mMessageView;
+	
+	private FlatButton mAcceptButton;
+	private FlatButton mCancelButton;
+	
+	private View.OnClickListener onAcceptButtonClickListener;
+	private View.OnClickListener onCancelButtonClickListener;
 
-	public SimpleDialog(Context context,String title, String message) {
-		super(context, title, message);
+
+
+	public SimpleDialog(Context context, String title, String message) {
+		super(context, R.layout.simple_dialog);
+		mContext = context;
+		mTitle = title;
+		mMessage = message;
+	}
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		mTitleView = (TextView) findViewById(R.id.title);
+	    mMessageView = (TextView) findViewById(R.id.message);
+	    mAcceptButton = (FlatButton) findViewById(R.id.button_accept);
+	    mCancelButton = (FlatButton)findViewById(R.id.button_cancel);
+	    
+	    setOnClickEventToButtons();
+	    setTitle(mTitle);
+	    setMessage(mMessage);
+
+	}
+	
+	
+	private void setOnClickEventToButtons() {
+		mAcceptButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dismiss();
+				if(onAcceptButtonClickListener != null)
+			    	onAcceptButtonClickListener.onClick(v);
+			}
+		});
+	    
+	    mCancelButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dismiss();
+				if(onCancelButtonClickListener != null)
+					onCancelButtonClickListener.onClick(v);
+			}
+		});
+	}
+	
+	
+	
+	public void setTitle(String title) {
+		if(null == title || title.length() == 0) {
+			mTitleView.setVisibility(View.GONE);
+		} else {
+			mTitleView.setVisibility(View.VISIBLE);
+			mTitleView.setText(title);
+		}
+	}
+	
+	public void setMessage(String message) {
+		if(null == message || message.length() == 0) {
+			mMessageView.setVisibility(View.GONE);
+		}else {
+			mMessageView.setVisibility(View.VISIBLE);
+			mMessageView.setText(message);
+		}
+	}
+	
+	public FlatButton getCancelButton() {
+		return mCancelButton;
+	}
+	
+	public FlatButton getAcceptButton() {
+		return mAcceptButton;
+	}
+	
+	public void setAcceptText(String text) {
+		mAcceptButton.setText(text);
+	}
+
+	public void setCancelText(String text) {
+		mCancelButton.setText(text);
+	}
+	
+	public void setOnAcceptButtonClickListener(View.OnClickListener onAcceptButtonClickListener) {
+		this.onAcceptButtonClickListener = onAcceptButtonClickListener;
+		if(mAcceptButton != null)
+			mAcceptButton.setOnClickListener(onAcceptButtonClickListener);
+	}
+
+	public void setOnCancelButtonClickListener( View.OnClickListener onCancelButtonClickListener) {
+		this.onCancelButtonClickListener = onCancelButtonClickListener;
+		if(mCancelButton != null)
+			mCancelButton.setOnClickListener(onCancelButtonClickListener);
 	}
 }
